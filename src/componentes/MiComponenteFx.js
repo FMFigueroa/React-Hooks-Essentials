19476e3abe041a17cc7a6cea1e9b1f769d70a204
Felix Figueroa <felixmanuelfigueroa@gmail.com>
2021-10-27 20:11:36 -0300

    Compartiendo el estado pero con un metodo creado.

--- src/componentes/MiComponenteFx.js
+++ src/componentes/MiComponenteFx.js
@@ -4,11 +4,29 @@ import Opcion from "./Option"
 export default function MiComponenteFx() {
   
   const [nombre, setNombre] = useState("Estado Inicial");
+  const [opciones, setOpciones] = useState([
+    {
+      name: "Opción 1",
+      value: 1,
+    },
+    {
+      name: "Opción 2",
+      value: 2,
+    },
+    {
+      name: "Opción 3",
+      value: 3,
+    },
+  ]);
+  //Metodo para la Primera propiedad:
+  const clickHandler = () => {
+    setNombre("Estado Actual")
+  }
 
 
   return (
     <div className="container row">
-      <Opcion name={nombre} clickHandler={ () => {setNombre("Estado Actual")}} />
+      <Opcion name={nombre} clickHandler={clickHandler} />
     </div>
   );
 }
