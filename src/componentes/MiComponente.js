aee9242d2a09cdef3b5db16be7809b64f545d620
Felix Figueroa <felixmanuelfigueroa@gmail.com>
2021-10-28 13:40:01 -0300

    Probando useEffect con useState

--- src/componentes/MiComponente.js
+++ src/componentes/MiComponente.js
@@ -1,18 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Micomponente () {
+
+  const [nombre,setNombre] = useState(1);
+
     
   useEffect( ()=> {
-    console.log("useEffect")
+    console.log(`Hola soy el useEffect numero ${nombre}`)
   })
 
   const metodo = () => {
-
+    setNombre(nombre +1)
   }
 
     return (
-      <div className="container row">
-        <h2 onClick={metodo}>Hook useEffect</h2>
+      <div className="container row" style={{display:'flex', flexDirection:'column',alignItems:'center'}}>
+        <h1>{`Hola soy el useEffect numero ${nombre}`}</h1>
+        <button onClick={metodo} >Add useEffect</button>
       </div>
     );
-}
+}  
