d91d0d5a8e470a883df35955f7b53a2c870628ff
Felix Figueroa <felixmanuelfigueroa@gmail.com>
2021-10-28 16:04:48 -0300

    Vincular Effects con propiedad especifica del estado

--- src/componentes/MiComponente.js
+++ src/componentes/MiComponente.js
@@ -1,24 +1,57 @@
 import React, { useEffect, useState } from "react";
 
-export default function Micomponente () {
+export default function Micomponente() {
+  const [conteo, setConteo] = useState(1);
+  const [opcion, setOpcion] = useState({
+    titulo: "texto",
+    valor: 1,
+  });
 
-  const [nombre,setNombre] = useState(1);
+  useEffect(() => {
+    console.log('test')
+    //setConteo(conteo + 1)
+  },[opcion.valor]);
 
-  //el effect lo podemos utilizar para tareas donde no vamos modificaar el estado. 
-  useEffect( ()=> {
-    console.log(`Hola soy el useEffect numero ${nombre}`)
-    document.title =  nombre>= 2 ? `effect (${nombre})`: `effect ${nombre}`;
-    //setNombre(nombre +1) NO CAMBIAR EL ESTADO DENTRO DEL EFFECT
-  })
+  const metodoUno = () => {
+    setOpcion( {...opcion, titulo: opcion.titulo + '.'})
+  };
+  const metodoDos = () => {
+    setOpcion( {...opcion, valor: opcion.valor + 1 })
+  };
 
-  const metodo = () => {
-    setNombre(nombre +1)
-  }
-
-    return (
-      <div className="container row" style={{display:'flex', flexDirection:'column',alignItems:'center'}}>
-        <h1>{`Hola soy el useEffect numero ${nombre}`}</h1>
-        <button onClick={metodo} >Add useEffect</button>
-      </div>
-    );
-}  
+  return (
+    <div
+      className="container row"
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h1>Vincular Effects con propiedades especificas del estado</h1>
+      <button onClick={metodoUno} style={{
+          marginTop: 20,
+          borderRadius: 50,
+          backgroundColor: '#f08e',
+          width: 100,
+          padding: 9,
+          color:'white',
+          //borderWidth:5,
+        }}>
+        Evento 1
+      </button>
+      <button
+        onClick={metodoDos}
+        style={{
+          marginTop: 20,
+          borderRadius: 10,
+          backgroundColor: "#ffa500",
+          width: 100,
+          padding:8,
+          color:'black',
+          fontSize:15,
+          fontWeight:'bold',
+          //borderWidth:5,
+        }}
+      >
+        Evento 2
+      </button>
+    </div>
+  );
+}
