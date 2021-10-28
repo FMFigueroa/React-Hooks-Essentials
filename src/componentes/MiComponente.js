d6926f7bf147a48e7fdd68804cd57d7398eb2e04
Felix Figueroa <felixmanuelfigueroa@gmail.com>
2021-10-28 17:49:40 -0300

    componentDidMount

--- src/componentes/MiComponente.js
+++ src/componentes/MiComponente.js
@@ -1,24 +1,21 @@
 import React, { useEffect, useState } from "react";
 
 export default function Micomponente() {
-  const [conteo, setConteo] = useState(0);
+  const [conteo, setConteo] = useState(1);
   const [opcion, setOpcion] = useState({
     titulo: 1,
     valor: 1,
   });
 
   useEffect(() => {
-    console.log('test')
-    setConteo( conteo + 1 ) // se ejecuta una sola vez y luego si cambia el estado de la propiedad opcion espeficicamente el valor.
-  },[opcion.valor]);  // esta dependencia rastrea el estado de una propiedad diferente, 
-                    //es util para controlar el cambio el estado de otra propiedad de forma controlada.
-
-  const metodoUno = () => {
+    console.log('test');
+    document.title= `Conteo: ${conteo}`;
+    
+  },[/* conteo */]);  
+   
+  const metodoDidMount = () => {
     setOpcion( {...opcion, titulo: opcion.titulo + 1})
-  };
-  const metodoDos = () => {
-    setOpcion( {...opcion, valor: opcion.valor +1 }) 
-    // cuando cabia su estado, useEffect lo detecta y cambia el estado de otra propiedad.
+    setConteo( conteo + 1 ) ;
   };
 
   return (
@@ -27,10 +24,8 @@ export default function Micomponente() {
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
       <h1>Vincular Effects con propiedades especificas del estado</h1>
-      <h3>{`Soy el Effect ${conteo -1 }`}</h3>
-      <h3>{`Soy el valor ${opcion.valor}` }</h3>
-      <h3>{`Soy el Titulo ${opcion.titulo}` }</h3>
-      <button onClick={metodoUno} style={{
+      <h3>{`Soy el estado ${opcion.titulo}`}</h3>
+      <button onClick={metodoDidMount} style={{
           marginTop: 20,
           borderRadius: 50,
           backgroundColor: '#f08e',
@@ -40,22 +35,6 @@ export default function Micomponente() {
           //borderWidth:5,
         }}>
         Evento 1
-      </button>
-      <button
-        onClick={metodoDos}
-        style={{
-          marginTop: 20,
-          borderRadius: 10,
-          backgroundColor: "#ffa500",
-          width: 100,
-          padding:8,
-          color:'black',
-          fontSize:15,
-          fontWeight:'bold',
-          //borderWidth:5,
-        }}
-      >
-        Evento 2
       </button>
     </div>
   );
