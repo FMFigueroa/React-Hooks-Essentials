4ed2074717d550f66a21e1c5b11f764e34a6c694
Felix Figueroa <felixmanuelfigueroa@gmail.com>
2021-10-28 19:44:47 -0300

    componentWillUnmount

--- src/componentes/MiComponente.js
+++ src/componentes/MiComponente.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { unmountComponentAtNode } from "react-dom";
 
 export default function Micomponente() {
   const [conteo, setConteo] = useState(1);
@@ -12,11 +13,25 @@ export default function Micomponente() {
   useEffect(() => {
     console.log('DidUpdate');
     document.title = `DidUpdate: ${conteo}`;
-  }); 
+  });
+
+  useEffect(() => {
+    console.log('WillUnmount');
+    document.title = 'Componente Montado';
+
+    return () => {
+      console.log('Componente Desmontado');
+      document.title = 'Componente Desmontado';
+    }
+  },[]);
    
   const metodo = () => {
     setConteo( conteo + 1 ) ;
   };
+
+  const eliminar = () => {
+    unmountComponentAtNode(document.getElementById('root'));
+  }
 
   return (
     <div
@@ -35,6 +50,17 @@ export default function Micomponente() {
           //borderWidth:5,
         }}>
         Evento 1
+      </button>
+      <button onClick={eliminar} style={{
+          marginTop: 20,
+          borderRadius: 50,
+          backgroundColor: '#5829',
+          width: 100,
+          padding: 9,
+          color:'white',
+          //borderWidth:5,
+        }}>
+        Desmontar
       </button>
     </div>
   );
