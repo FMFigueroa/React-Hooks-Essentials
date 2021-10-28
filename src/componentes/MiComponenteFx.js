6f9174579e71406409b909bc3bb7cf48496912c7
Felix Figueroa <felixmanuelfigueroa@gmail.com>
2021-10-27 22:26:58 -0300

    Modificamos el estado de un objeto en el array

--- src/componentes/MiComponenteFx.js
+++ src/componentes/MiComponenteFx.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import Opcion from "./Option"
+import Opcion from "./Option";
 
 export default function MiComponenteFx() {
-  
   const [nombre, setNombre] = useState("1");
 
   const [direccion, setDireccion] = useState({
@@ -10,7 +9,7 @@ export default function MiComponenteFx() {
     estado: "Arizona",
     ciudad: "Springfileld",
   });
-  
+
   const [opciones, setOpciones] = useState([
     {
       name: "Opción 1",
@@ -27,24 +26,32 @@ export default function MiComponenteFx() {
   ]);
 
   const cambio = () => {
-
+    //String
     //setNombre('2');
 
-    //spread operator son tres puntos: "..."
-    setDireccion({...direccion, estado: "Carabobo"})
-  }
+    //Objeto
+    //spread operator accede a todos los elementos de un opjeto y lo replica,
+    // se represeenta con tres puntos: "..."
+    //setDireccion({...direccion, estado: "Carabobo"})
+
+    //Array
+    const newObjeto = {
+      name: "Nueva opcion",
+      value: "Nuevo valor",
+    };
+
+    const actualizado = opciones.map((item) =>
+      item.value === 3 ? newObjeto : item
+    );
+    setOpciones(actualizado);
+  };
 
   //Metodo para mapear una array:
-  const listaOpciones = opciones.map(({name, value}) => (
-    <div key={name.replace("","").toLowerCase()} className="col-md-4">
-      <Opcion name={name} clickHandler={cambio}/>
+  const listaOpciones = opciones.map(({ name, value }) => (
+    <div key={name.replace("", "").toLowerCase()} className="col-md-4">
+      <Opcion name={name} clickHandler={cambio} />
     </div>
-
   ));
 
-  return (
-    <div className="container row">
-      {listaOpciones} 
-    </div>
-  );
+  return <div className="container row">{listaOpciones}</div>;
 }
