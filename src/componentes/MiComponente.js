e9ca81b93118e0e7a269b2adbaa2ffde8bf7569d
Felix Figueroa <felixmanuelfigueroa@gmail.com>
2021-10-28 16:59:52 -0300

    Controlando el cambio de estado de diferentes propiedades vinculadas en useEffect

--- src/componentes/MiComponente.js
+++ src/componentes/MiComponente.js
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 export default function Micomponente() {
-  const [conteo, setConteo] = useState(1);
+  const [conteo, setConteo] = useState(0);
   const [opcion, setOpcion] = useState({
-    titulo: "texto",
+    titulo: 1,
     valor: 1,
   });
 
   useEffect(() => {
     console.log('test')
-    //setConteo(conteo + 1)
-  },[opcion.valor]);
+    setConteo( conteo + 1 ) // se ejecuta una sola vez y luego si cambia el estado de la propiedad opcion espeficicamente el valor.
+  },[opcion.valor]);  // esta dependencia rastrea el estado de una propiedad diferente, 
+                    //es util para controlar el cambio el estado de otra propiedad de forma controlada.
 
   const metodoUno = () => {
-    setOpcion( {...opcion, titulo: opcion.titulo + '.'})
+    setOpcion( {...opcion, titulo: opcion.titulo + 1})
   };
   const metodoDos = () => {
-    setOpcion( {...opcion, valor: opcion.valor + 1 })
+    setOpcion( {...opcion, valor: opcion.valor +1 }) 
+    // cuando cabia su estado, useEffect lo detecta y cambia el estado de otra propiedad.
   };
 
   return (
@@ -25,6 +27,9 @@ export default function Micomponente() {
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
       <h1>Vincular Effects con propiedades especificas del estado</h1>
+      <h3>{`Soy el Effect ${conteo -1 }`}</h3>
+      <h3>{`Soy el valor ${opcion.valor}` }</h3>
+      <h3>{`Soy el Titulo ${opcion.titulo}` }</h3>
       <button onClick={metodoUno} style={{
           marginTop: 20,
           borderRadius: 50,
